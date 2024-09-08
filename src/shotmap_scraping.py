4d90b1d957104fbc605a0c05b1cf7d7d9a05184d
Vinicius Sales <vinicius.sales1998@gmail.com>
2024-09-07 22:53:12 -0300

    shots csv

--- src/shotmap_scraping.py
+++ src/shotmap_scraping.py
@@ -1,18 +1,25 @@
-# %%%
+# %%
 import requests
 import pandas as pd
 import json
-# %%
 
 df_matches = pd.DataFrame()
+df_shot = pd.DataFrame()
 
+#json matches
 with open('colunas_matches.json', 'r') as open_file:
     config = json.load(open_file)
 
 colunas_matches_mapping = config['colunas_matches_rename']
 colunas_matches_keep = config['colunas_matches_keep']
 
-# %%
+#json shots
+with open('colunas_shot.json', 'r') as open_file:
+    config = json.load(open_file)
+
+colunas_shot_mapping = config['colunas_shot_rename']
+colunas_shot_keep = config['colunas_shot_keep']
+
 #Puxando jogos da rodada para descobrir os IDs das partidas
 for i in range(1,16,1):
     url = f'https://www.sofascore.com/api/v1/unique-tournament/325/season/58766/events/round/{i}'
@@ -42,7 +49,6 @@ for i in range(1,16,1):
 
     df_round_matches = pd.json_normalize(matches)
 
-    id_matches = df_round_matches['id'].tolist()
 
     df_round_matches = df_round_matches.rename(columns=colunas_matches_mapping)
     df_round_matches = df_round_matches[colunas_matches_keep]
@@ -50,7 +56,47 @@ for i in range(1,16,1):
 
     df_matches = pd.concat([df_matches,df_round_matches], ignore_index=True)
 
-# %%
+
 matches_data_path = '../data/matches.csv'
 df_matches.to_csv(matches_data_path,sep=';')
+
+
+id_matches = df_matches['idPartida'].tolist()
+
+#Puxando informações de chutes
+
+headers = {
+    "accept": "*/*",
+    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,ceb;q=0.6",
+    "cache-control": "max-age=0",
+    "cookie": "_cc_id=248bb0221b87d759694ba698b54adb82; cto_bundle=ZJ-x9F9xOWQ0Znc3aXlNN1JUYVh1SSUyQmpuOUU0ZjFERnczMXB3b1ZXJTJCa1pWMHZ4TWV2T0VoNXZxT1N0dDZYJTJGTFlDY09nY3lvOW1CN3lVbHdLbFlNZVlwNktmdU9VVnk0MmhRJTJGU0EwckpmQWZ3JTJGeGFGQ1NEUE92VHdLZHRzTnRIVTlmWTZVUlc1bllTcXRDenhSeXFFak5TQ0Z3JTNEJTNE; cto_bidid=QrAq_l9kbzFQclFTUmwzZSUyRnVYYnBGaUdUQ0NIMVRoU2klMkJ6T1I5cXclMkJtQ1h2MDVLd2Q0Q05lbyUyQmtkdDh2UCUyRmFYalRIN3IzWmxDbDlHWnRoOFdDSXlGcmFTbWlka1pCbDlRSnElMkIwTCUyRmNDSFNESW9ZJTNE; _ga=GA1.1.398452961.1683470122; _scid_r=21548dbe-6221-424a-b03c-757c92301a30; cto_bundle=V2OTq19xOWQ0Znc3aXlNN1JUYVh1SSUyQmpuOUVFY2tRR3NMbDdxZ1BpeHhwTTRGWUNMbkw4SHJuaVMyQlBxdnR3ZEJCc2F1Uk1iSFkydld5d2lEWkZycm80bWQwWEV1dlBCdDRtSkhrYjJ3QzlyU3ZRb1VBNU4lMkJIN0Q3VjZKRnpQbW9Td1M0RGY1RUwwOG14SjZWSzNjNW05UWFBJTNEJTNE; cto_bidid=cCYi-19kbzFQclFTUmwzZSUyRnVYYnBGaUdUQ0NIMVRoU2klMkJ6T1I5cXclMkJtQ1h2MDVLd2Q0Q05lbyUyQmtkdDh2UCUyRmFYalRIN3IzWmxDbDlHWnRoOFdDSXlGcmFTbXZHbG94M04zM0E2U2xJdTRCeFhpRTglM0Q; __gads=ID=92550bb856c582d6:T=1692480711:RT=1710383147:S=ALNI_MZrT1s8MHP0mcaFqh0rt4eA5nyg2g; __gpi=UID=00000d9ee914ece0:T=1692480711:RT=1710383147:S=ALNI_MbYN2sUykqmarIVIdzFBSoVXp4Bgw; _lr_env_src_ats=false; _clck=y6mv2b|2|fny|0|1669; gcid_first=05ebc6a3-58f6-48b6-8393-c1affef71376; _ga_QH2YGS7BB4=GS1.1.1724291547.164.1.1724293598.0.0.0; _ga_3KF4XTPHC4=GS1.1.1724291547.134.1.1724293598.56.0.0; FCNEC=[['AKsRol8KP1wNwlD_JrKUt8mb2yLatXFvLSCp269TUorqUf5EmSiUJodLz8xh3_JJFbUMixEMBlFwwE7lkVKPW3EEo2onn8B8N1_M5ZUEXzksFXjaJVZ9vOsOnHPAril_7NXGaDpt91EIXtiycg7iGwuKJ_VXCwAucQ=='],['AKsRol8KP1wNwlD_JrKUt8mb2yLatXFvLSCp269TUorqUf5EmSiUJodLz8xh3_JJFbUMixEMBlFwwE7lkVKPW3EEo2onn8B8N1_M5ZUEXzksFXjaJVZ9vOsOnHPAril_7NXGaDpt91EIXtiycg7iGwuKJ_VXCwAucQ==']]",
+    "priority": "u=1, i",
+    "referer": "https://www.sofascore.com/football/match/criciuma-juventude/FOsJO",
+    "sec-ch-ua": "'Chromium';v='128', 'Not;A=Brand';v='24', 'Google Chrome';v='128'",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "'Windows'",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+    "x-requested-with": "60ac73"
+}
+
+for game in id_matches:
+    url = f"https://www.sofascore.com/api/v1/event/{game}/shotmap"
+    response = requests.request("GET", url, headers=headers)
+    shot = response.json()['shotmap']
+    df_shot_matches = pd.json_normalize(shot)
+
+    df_shot_matches['idPartida'] = game
+
+    df_shot_matches = df_shot_matches.rename(columns=colunas_shot_mapping)
+    df_shot_matches = df_shot_matches[colunas_shot_keep]
+
+
+    df_shot = pd.concat([df_shot,df_shot_matches], ignore_index=True)
+
+shot_data_path = '../data/shot.csv'
+df_shot.to_csv(shot_data_path,sep=';')
+
 # %%
